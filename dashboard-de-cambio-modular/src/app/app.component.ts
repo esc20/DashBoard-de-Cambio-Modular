b421eab6caa085bd1360ab223cd27ccba510cf72
esc20 <161521615+esc20@users.noreply.github.com>
2026-03-19 23:00:14 -0300

    Um excelente dia de trabalho

--- dashboard-de-cambio-modular/src/app/app.component.ts
+++ dashboard-de-cambio-modular/src/app/app.component.ts
@@ -11,5 +11,9 @@ import { CardComponent } from './components/card/card.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+   moedas = [
+    { nome: 'Brasil', sigla: 'BRL', valor: 1.00, tendencia: 'estavel', bandeira: 'Flag_of_Brazil.svg' },
+    { nome: 'EUA', sigla: 'USD', valor: 5.40, tendencia: 'subindo', bandeira: 'Flag_of_USA.svg' },
+    { nome: 'Japão', sigla: 'JPY', valor: 0.035, tendencia: 'caindo', bandeira: 'Flag_of_Japan.svg' }
+  ];
 }
--- dashboard-de-cambio-modular/src/app/components/card/card.component.ts
+++ dashboard-de-cambio-modular/src/app/components/card/card.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
-  @Input() titulo: string = "";
-  @Input() valor: string = "";
+   moedas = [
+    { nome: 'Brasil', sigla: 'BRL', valor: 1.00, tendencia: 'estavel', bandeira: 'Flag_of_Brazil.svg' },
+    { nome: 'EUA', sigla: 'USD', valor: 5.40, tendencia: 'subindo', bandeira: 'Flag_of_the_United_States.svg' },
+    { nome: 'Japão', sigla: 'JPY', valor: 0.035, tendencia: 'caindo', bandeira: 'Flag_of_Japan.svg' },
+    { nome: 'Alemanha', sigla: 'DEU', valor: 4.35, tendencia: 'caindo', bandeira: 'Flag_of_Germany.svg.png' },
+    { nome: 'Grã-Bretanha', sigla: 'UKX', valor: 1.50, tendencia: 'estavel', bandeira: 'Flag_of_Great_Britain_(1707–1800).svg.png'},
+    { nome: 'China', sigla: 'CHN', valor: 2.60, tendencias: 'subindo', bandeira: 'Flag_of_China.jpg'} 
+  ];
 }
